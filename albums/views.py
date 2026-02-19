e6c5035bec52184514e5ff19a05a901d97115ba5
MonsterGuti <gogulanovmartin@gmail.com>
2026-02-19 13:57:31 +0200

    created album view

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from albums.forms import AlbumCreateForm
+from albums.models import Album
+from profiles.util import get_profile
+
+
+class AlbumCreateView(CreateView):
+    model = Album
+    form_class = AlbumCreateForm
+    success_url = reverse_lazy('profiles:home')
+    template_name = 'albums/album-add.html'
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['owner'] = get_profile()
+        return initial
+
+    def form_valid(self, form):
+        form.instance.owner = get_profile()
+        return super().form_valid(form)
