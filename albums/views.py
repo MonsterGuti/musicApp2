8e4be9cf0c9ed196d8742f25199d0abb6c0ca6cf
MonsterGuti <gogulanovmartin@gmail.com>
2026-02-19 14:28:22 +0200

    feat: created edit album view

--- albums/views.py
+++ albums/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
-from albums.forms import AlbumCreateForm
+from albums.forms import AlbumCreateForm, AlbumEditForm
+from albums.mixins import AttachOwnerMixin
 from albums.models import Album
 from profiles.util import get_profile
 
 
-class AlbumCreateView(CreateView):
+class AlbumCreateView(AttachOwnerMixin, CreateView):
     model = Album
     form_class = AlbumCreateForm
     success_url = reverse_lazy('profiles:home')
     template_name = 'albums/album-add.html'
 
-    def get_initial(self):
-        initial = super().get_initial()
-        initial['owner'] = get_profile()
-        return initial
 
-    def form_valid(self, form):
-        form.instance.owner = get_profile()
-        return super().form_valid(form)
+class AlbumDetailView(DetailView):
+    model = Album
+    template_name = 'albums/album-details.html'
+
+
+class AlbumEditView(AttachOwnerMixin, UpdateView):
+    model = Album
+    form_class = AlbumEditForm
+    success_url = reverse_lazy('profiles:home')
+    template_name = 'albums/album-edit.html'
+
+    def get_success_url(self):
+        return reverse('albums:details', kwargs={'pk': self.object.pk})
